c859db5c9e0c3dd0714358eb8f6dbf81c64e967e
tonakai-s <rdorneles64@gmail.com>
2025-02-26 21:32:50 -0300

    setup tests to bind a random port chosed by OS

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,16 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health-check")
+        .get(format!("{}/health-check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -18,10 +20,14 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod_rust::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod_rust::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
